102aec92395618e7be169ec8c45b2e8a801df96d
Yunst <71825961+yunst047@users.noreply.github.com>
2025-12-02 15:25:13 +0700

    init: go project, consumer and producer. docker compose for running RabbitMQ.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-rabbitmq-practice
+
+go 1.24.5
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
---
+++ producer/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	// 1. Connect to RabbitMQ
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// 2. Open a Channel (Most API operations happen on a channel, not the connection)
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// 3. Declare a queue (idempotent: created only if it doesn't exist)
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable (persist after server restart?)
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// 4. Publish a message
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange (empty string = default exchange)
+		q.Name, // routing key (queue name)
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
